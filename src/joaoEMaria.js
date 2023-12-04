e0133c6237d1c542e2c6bc28a983cb34269fc531
Vinícius F.G Melo <viniciusferrazgmelo@outlook.com>
2023-12-03 21:21:08 -0300

    att

--- src/joaoEMaria.js
+++ src/joaoEMaria.js
@@ -1,40 +1,40 @@
-console.log('módulo joaoEMaria');
+console.log('módulo JoaoEMaria');
 
-let maria = 2;
 let joao = 1;
+let maria = 2;
 
-function getMaria() {
-  return maria;
+function getMaria(){
+    return maria;
 }
 
-function getJoao() {
-  return joao;
+function getJoao(){
+    return joao;
 }
 
-function setJoao(novoValor) {
-  if (novoValor > 0) {
-    joao = novoValor;
-  } else {
+function setJoao(novoValor){
+    if(novoValor > 0){
+        joao = novoValor;
+    } else {
+       joao = 0;
+    }
+}
+
+function setMaria(novoValor){
+    if(novoValor > 0){
+        maria = novoValor;
+    } else {
+        maria = 0;
+    }
+}
+
+function deJoaoParaMaria(){
+    maria = maria + joao;
     joao = 0;
-  }
 }
 
-function setMaria(novoValor) {
-  if (novoValor > 0) {
-    maria = novoValor;
-  } else {
-    maria = 0;
-  }
+function deMariaParaJoao(){
+    joao = joao + maria;
+    maria = 0; 
 }
 
-function deJoaoParaMaria() {
-  maria = maria + joao;
-  joao = 0;
-}
-
-function deMariaParaJoao() {
-  joao = joao + maria;
-  maria = 0;
-}
-
-export { getJoao, getMaria, setMaria, setJoao, deJoaoParaMaria, deMariaParaJoao };
+export {getJoao, getMaria, setJoao, setMaria, deJoaoParaMaria, deMariaParaJoao}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { deJoaoParaMaria, getJoao, getMaria} from "./joaoEMaria";
+import { deJoaoParaMaria,deMariaParaJoao, getJoao, getMaria } from "./joaoEMaria.js";
 
-// main.js
 console.log('módulo main');
 
-console.log('João tem ', getJoao(), ' maçãs!');
-console.log('Maria tem ', getMaria(), ' maçãs!');
+console.log('João tem ', getJoao(),' maçãs!');
+console.log('Maria tem ', getMaria(),' maçãs!');
 
 deJoaoParaMaria();
 
-console.log('João tem ', getJoao(), ' maçãs!');
-console.log('Maria tem ', getMaria(), ' maçãs!');
+console.log('João tem ', getJoao(),' maçãs!');
+console.log('Maria tem ', getMaria(),' maçãs!');
